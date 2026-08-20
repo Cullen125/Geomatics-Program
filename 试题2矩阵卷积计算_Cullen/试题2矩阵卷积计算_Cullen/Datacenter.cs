7e019fdbd017336f02f458988a18b2674e9ccf74
Sedrick <2894607195@qq.com>
2026-08-20 10:12:04 +0800

    Add files via upload

---
+++ 试题2矩阵卷积计算_Cullen/试题2矩阵卷积计算_Cullen/Datacenter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 试题2矩阵卷积计算_Cullen
+{
+    static class Datacenter
+    {
+        public static double[,] N = new double[10, 10];
+        public static double[,] M = new double[3, 3];
+
+        public static double[,] V1 = new double[10, 10];
+        public static double[,] V2 = new double[10, 10];
+
+        public static bool hasN = false;
+        public static bool hasM = false;
+
+        public static string report = "";
+    }
+}
