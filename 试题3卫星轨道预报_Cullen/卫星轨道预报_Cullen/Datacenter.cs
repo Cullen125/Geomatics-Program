056b8e3e37976a0005b9a4ce07582b79bde7ee07
Sedrick <2894607195@qq.com>
2026-08-20 10:13:03 +0800

    Add files via upload

---
+++ 试题3卫星轨道预报_Cullen/卫星轨道预报_Cullen/Datacenter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 卫星轨道预报_Cullen
+{
+    internal class Orbit
+    {
+        public double t;
+        public double x;
+        public double y;
+        public double z;
+    }
+
+    internal class Predict
+    {
+        public double t;
+        public double x;
+        public double y;
+        public double z;
+    }
+
+    internal static class Datacenter
+    {
+        static public List<Orbit> orbits = new List<Orbit>();
+        static public List<Predict> predicts = new List<Predict>();
+
+        static public double b0x;
+        static public double b1x;
+
+        static public double b0y;
+        static public double b1y;
+
+        static public double b0z;
+        static public double b1z;
+
+        static public string report = "";
+
+    }
+}
---
+++ 试题3卫星轨道预报_Cullen/卫星轨道预报_Cullen/FileHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 卫星轨道预报_Cullen
+{
+    internal static class FileHelper
+    {
+        static public void OpenFile()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "文本数据|*.txt";
+            op.Title = "打开文本数据";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                Datacenter.orbits.Clear();
+
+                StreamReader sr= new StreamReader(op.FileName);
+                sr.ReadLine();
+
+                while (! sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    
+                    string[] parts = line.Split(new char[] { ',', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    double t = double.Parse(parts[0]);
+                    double x = double.Parse(parts[1]);
+                    double y = double.Parse(parts[2]);
+                    double z = double.Parse(parts[3]);
+
+                    Orbit o = new Orbit();
+                    o.t = t;
+                    o.x = x;
+                    o.y = y;
+                    o.z = z;
+
+                    Datacenter.orbits.Add(o);
+                }
+                sr.Close();
+            }
+            
+        }
+
+        static public void saveReport(string report)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "文本数据|*";
+            sf.Title = "选择保存路径";
+            sf.FileName = "result";
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sf.FileName);
+                sw.Write(report);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+}
